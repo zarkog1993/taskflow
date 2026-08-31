232ef500b55ae169d0c0b3bc786b6e612099f589
Zarko Gavric <zarko.gavric@sportserve.co>
2026-08-31 09:58:36 +0200

    Created axios client for authorization with bearer token

---
+++ taskflow-frontend/src/api.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: 'http://localhost:8080/api',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }
+})
+
+// Request Interceptor: automatski dodaje Sanctum token
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+// Response Interceptor: obrada 401 grešaka (istečen token)
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem('token')
+      window.location.href = '/login'
+    }
+    return Promise.reject(error)
+  }
+)
+
+export default api
